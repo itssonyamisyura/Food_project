e12ed68073076590741c7e8e4ee023f8c7e22556
Sofia <itssonyamisyura@gmail.com>
2025-11-05 20:41:04 +0100

    Запускаем JSON-server

--- js/script.js
+++ js/script.js
@@ -312,4 +312,9 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json()) // берем ответ от сервера и превращаем в js объект
+        .then(res => console.log(res)); // результат в консоль
+
 });// назначение глобального обработчика событий
