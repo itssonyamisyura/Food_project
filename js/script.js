a1c8539997b34a901cca175d6e866f8a506dab01
Sofia <itssonyamisyura@gmail.com>
2025-11-20 20:24:36 +0100

    Создаем калькулятор на сайте, часть 3 -LocalStorage - запоминание выбранных параметров при заходе на сайт

--- js/script.js
+++ js/script.js
@@ -474,9 +474,45 @@ window.addEventListener('DOMContentLoaded', () => {
     // calc
 
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female',
-        height, weight, age,
+
+    let sex, height, weight, age, ratio;
+    // if we have info in localStorage -> we use it, if no info-> по умолчанию
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', 'female');
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
         ratio = 1.375;
+        localStorage.setItem('ratio', 1.375);
+    }
+
+    // классы активности как в localStorage
+    function initLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+        
+        // убираем классы активности, назначаем класс активности эл из localstorage
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+             // id
+            if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+                elem.classList.add(activeClass);
+            } 
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass);
+            }
+        });
+    }
+     
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
+
+
 
     function calcTotal() {
         if (!sex || !height || !weight || !age || !ratio) {
@@ -494,15 +530,17 @@ window.addEventListener('DOMContentLoaded', () => {
     calcTotal()
 
 
-    function getStaticInformation(parentSelector, activeClass) {
-        const elements = document.querySelectorAll(`${parentSelector} div`);
+    function getStaticInformation(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
 
         elements.forEach(elem => {
             elem.addEventListener('click', (e) => {
                 if (e.target.getAttribute('data-ratio')) {
                     ratio = +e.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
                 } else {
                     sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', e.target.getAttribute('id'));
                 }
     
                 // убираем классы активности у всех эл.
@@ -517,8 +555,9 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    getStaticInformation('#gender', 'calculating__choose-item_active');
-    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    getStaticInformation('#gender div', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
+    // div --> так как мы обращаемся к блокам внутри селектора
 
     function getDynamicInformation(selector) {
         const input = document.querySelector(selector);
